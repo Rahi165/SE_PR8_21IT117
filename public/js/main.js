1892a4f2dde1537656110bfb973d9c10c384b3e4
Rahi <97022750+Rahi165@users.noreply.github.com>
2024-10-03 23:01:22 +0530

    Delete public directory

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-import GameController from './controller/game-controller.js';
-
-const gameController = new GameController();
-/* global io */
-// io is a global variable for socket.io-client set from the view html
-gameController.connect(io);
